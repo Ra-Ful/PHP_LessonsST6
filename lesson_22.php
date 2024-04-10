abdd4d1805a7eb4def3a217e62094448846b4673
Sai.An <145052137+Ra-Ful@users.noreply.github.com>
2024-04-10 19:51:30 +0500

    Update lesson_22.php

--- lesson_22.php
+++ lesson_22.php
@@ -8,13 +8,12 @@
 <body>
 
 <?php 
-
+//Ошибки по причине другого названия базы данных и таблицы
 $action ="";
 if (isset($_GET['action'])) {
     $action=$_GET['action'];
 };
 var_dump($_GET);
-
 echo '<hr>';
 if($action=="create_new"){
     //Создаем новую запись в базе данных
@@ -33,34 +32,74 @@ if ($action=="done"){
         $sub_key = substr($key,0,8);
         if($sub_key=="id_done_"){
             $id=substr($key,8);
-            echo "<hr> выполнено... id=".$id."<br>";
+            echo "<hr> Выполнено... id=".$id."<br>";
+            //update
+            $sql = "UPDATE `todo` SET complete=1 WHERE id=?";
+            $conn = new mysqli("localhost","root","","to do", 3306);
+            $conn->set_charset("utf8");
+            if($conn->connect_error){
+                die("Ошибка: ".$conn->connect_error);
+            }
+            $stmt = $conn -> prepare($sql);
+            $stmt -> bind_param("s",$id);
+            $stmt -> execute();
+        }
+    }
+}
+
+
+if ($action =="delete"){
+    foreach($_GET as $key => $value){
+        $sub_key = substr($key, 0,8);
+        if ($sub_key=="id_done_"){
+            $id = substr($key, 8);
+            echo "<hr> Удаление..id=".$id."<br>";
+            $sql = "DELETE FROM `todo` WHERE id=?";
+            $conn= new mysqli("localhost", "root","","to do", 3306);
+            $conn->set_charset("utf8");
+            if($conn->connect_error){
+                die("Ошибка:".$conn->$connect_error);
+            }
+            $stmt = $conn -> prepare($sql);
+            $stmt-> bind_param("s",$id);
+            $stmt->execute();
         }
     }
 }
 
 ?>
-    
+
     <form>
     <input type="text" name="new_do"> <button type="submit" name="action" value="create_new">Создать новый</button><br />
     <ul>
-        <?php
-        $sql = "SELECT * FROM todo";
-        $conn = new mysqli("localhost", "root", "","to do",3306);
-        $conn->set_charset("utf8");
-        if ($conn->connect_error) { 
-            die("Ошибка: ".$conn->connect_error); }
-            $stmt = $conn->prepare($sql);
-            //$stmt->bind_param("i",$offset);
-            $stmt =$conn ->bind_param("s",$text);
-
-            $stmt->execute();
-            $result = $stmt->get_result();
+    <?php
+    $sql = "SELECT * FROM `todo` ORDER BY complete";
+    $conn = new mysqli("localhost","root","","to do",3306);
+    $conn->set_charset("utf8");
+    if ($conn->connect_error){
+        die("Ошибка: ". $conn->connect_error);
+    }
+        $stmt =$conn->prepare($sql);
+        //$stmt->bind_param("i",$offset);
+        $stmt->execute();
+        $result = $stmt->get_result();
             if ($result) {
                 foreach ($result as $row) {
-                    echo "<li>";
-                    echo '<input type="checkbox" name="id_done_'.$row['id'].'" value="1">';
-                    echo $row['text'];
-                    echo "</li>";
+                    if($row['complete'==1]){
+                        //Выполнено
+                        echo "<li>";
+                        echo '<input type="checkbox" name="id_done_'.$row['id'].'" value="1">';
+                        echo '<s>'.htmlentities($row['text'], ENT_QUOTES, 'UTF-8').'</s>';
+                        echo "</li>";
+                        
+                    }
+                    else {
+                        //не выполнено
+                        echo "<li>";
+                        echo '<input type="checkbox" name="id_done_'.$row['id'].'"value="1">';
+                        echo htmlentities($row['text'], ENT_QUOTES, 'UTF-8');
+                        echo "</li>";
+                    }
                 }
             }
         
@@ -68,7 +107,6 @@ if ($action=="done"){
    
     <button type="submit" name="action" value="done">Выполнены</button>
     <button type="submit" name="action" value="delete">Удалить</button>
-
 </form>
 
 </body>
