b39c529b0798c7137fa60a6dc543b86463a372bd
Sai.An <145052137+Ra-Ful@users.noreply.github.com>
2024-04-19 19:49:52 +0500

    Update index.php

--- TODO/index.php
+++ TODO/index.php
@@ -1,22 +1,11 @@
-<?php
+<ul><?php
  include ("todo_class.php");
  $todo=new Todo();
- $todo->show();
-
- $list = $todo->select();
- if($list){
-    foreach ($list as $row){
-        ?> 
-        <li><?=$row["id"];?>. <?=htmlentities($row["Text"], ENT_QUOTES,'UTF-8');?></li>
-        <?php
-
-
-    }
- }
-
-
-
-
-
-
-?>
+ ob_start();
+require "view/list.php";
+$CONTENT = ob_get_clean();
+$main = file_get_contents("view/template/main.tpl");
+$main =str_replace('{HEADER}',"Заголовок",$main);
+$main =str_replace('{TITLE}',"Заголовок страницы", $main);
+$main =str_replace('{SOME_CONTENT}',$CONTENT, $main);
+echo $main;
